e9bd108746eb4e9ec9b0cf24812cc8ad64f95af4
elis-li <petrushina.2014@mail.ru>
2024-08-06 16:09:10 +0300

    Changes

--- android/app/src/main/kotlin/com/example/untitled1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/untitled1/MainActivity.kt
@@ -1,11 +1,3 @@
 package com.example.untitled1
 
-import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
-
-    override fun onCreate(savedInstanceState: Bundle!) {
-        WindowCompat.setDecorFitsSestemWindows(window, false)
-        super.onCreate(savedOnstanceState)
-    }
-    }
---
+++ lib/dedsign/dialog/error_dialog.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class ErrorDialog extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
